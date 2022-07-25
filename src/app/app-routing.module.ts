1c6f3d0ad0013df781e0e515ae7aca4cf4e7dd98
BrianPelaez <pelaez_brian@hotmail.com>
2022-07-25 16:59:33 -0300

    Path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component'
 import { Error404Component } from './error404/error404.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
 
 // IMPORT COMPONENT END //
 
@@ -12,9 +13,11 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent}, // indicar la ruta y el componente 
   {path: 'login', component: LoginComponent},
   {path: 'contact', component: ContactoComponent},
+  {path: 'home', component: HomeComponent},
+
 
   //{path: 'profile', component: LoginComponent, canActive: [AuthGuard]}, -> Solo accesible si el user esta logueado
-  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: Error404Component}, // Error 404
 ];
 
