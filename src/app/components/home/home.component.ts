d32d56d7170b89030f14095390add324129cec8d
Brian Pelaez <pelaez_brian@hotmail.com>
2022-09-09 15:42:10 -0300

    Responsive

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 import { PortafolioService } from 'src/app/services/portafolio.service';
-
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   portafolioData: any;
   expList: any;
-  
 
-  constructor() { 
-  }
+  constructor() {}
 
   ngOnInit(): void {
+    const options = {
+      strings: ['Desarrollador', 'Full-Stack Dev.', 'Programador', 'Soporte IT'],
+      typeSpeed: 40,
+      backSpeed: 40,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true,
+    };
 
+    const typed = new Typed('.typing', options);
   }
-
 }
